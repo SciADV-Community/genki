70ea1ffe2c45203909f902af140faa7cd4f911d1
evangelos-ch <angelos.h@hotmail.com>
2020-10-15 05:32:20 +0100

    Fix bug in media protection.

--- playthrough/views.py
+++ playthrough/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import get_object_or_404
 @login_required(login_url='/login')
 def serve_archive(request, filename):
     archive = get_object_or_404(Archive, file__exact=f'protected/{filename}')
-    if archive.users.filter(id=request.user.id).exists():
+    if not archive.users.filter(id=request.user.id).exists():
         return HttpResponse("Unauthorized", status=403)
 
     response = HttpResponse()
