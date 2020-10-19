6ee52d2241a075a3f9c65749baecbcb1b031e616
evangelos-ch <angelos.h@hotmail.com>
2020-10-19 01:28:31 +0100

    Added output to the migration script.

--- playthrough/management/commands/migrate_shogun.py
+++ playthrough/management/commands/migrate_shogun.py
@@ -29,28 +29,42 @@ class Command(BaseCommand):
         for guild in guilds_in_db:
             Guild.objects.get_or_create(id=guild[0], name=guild[1])
         # Migrate Games
+        self.stdout.write('- Migrating games...')
         c.execute('SELECT name, channel_suffix, role_name FROM Game')
         games_in_db = c.fetchall()
+        self.stdout.write(f'- - Found {len(games_in_db)} games.')
         for game in games_in_db:
+            self.stdout.write(f'- - Migrating {game[0]}')
             role_template = RoleTemplate.objects.create(name=game[2])
             game = Game.objects.get_or_create(
-                name=game[0], channel_suffix=f'-plays-{game[1]}', completion_role=role_template
+                name=game[0]
             )[0]
+            game.channel_suffix = f'-plays-{game[1]}'
+            game.completion_role = role_template
+            game.save()
+            self.stdout.write(f'- - Saved {game[0]}.')
             # Migrate Aliases
             c.execute('SELECT alias FROM Game_Alias WHERE game_name = ?', (game.name,))
             aliases = c.fetchall()
+            self.stdout.write(f'- - - Found {len(aliases)} aliases.')
             for alias in aliases:
                 game.aliases.add(Alias(alias=alias[0]), bulk=False)
+            self.stdout.write('- - - Migrated aliases.')
             # Migrate Configs
             c.execute('SELECT Guild_Id FROM Game_Guild WHERE Game_Name = ?', (game.name,))
             configs = c.fetchall()
+            self.stdout.write(f'- - - Found {len(configs)} GameConfigs.')
             for config in configs:
+                self.stdout.write(f'- - - Migrating {game} - {config[0]}.')
                 GameConfig.objects.get_or_create(
                     guild_id=config[0], game=game, playable=True, completion_role_id='000000000000'
                 )
             # Migrate Channels
             c.execute('SELECT ID, Owner, Guild FROM Channel WHERE Game = ?', (game.name,))
             channels = c.fetchall()
+            self.stdout.write(f'- - - Found {len(channels)} channels.')
             for channel in channels:
+                self.stdout.write(f'- - - Migrating {channel[0]}...')
                 user = User.objects.get_or_create(id=channel[1])[0]
                 Channel.objects.get_or_create(id=channel[0], owner=user, guild_id=channel[2], game=game)
+                self.stdout.write(f'- - - Migrated {channel[0]}.')
