307d5397c356df44287c0a3e8f765cf6c7cfd0e7
Evangelos Ch <angelos.h@hotmail.com>
2022-03-13 12:42:36 +0000

    Hopefully CI/CD, Touch up on the DaSH migration script

--- playthrough/management/commands/migrate_dash.py
+++ playthrough/management/commands/migrate_dash.py
@@ -7,6 +7,7 @@ from django.core.management.base import BaseCommand
 from playthrough.models import Alias, Channel, Game, GameConfig, Guild, RoleTemplate, User
 
 class Command(BaseCommand):
+    help = 'Migrates a DB from \'DaSH\' bot (legacy bot used in Gate of Zero).'
 
     @staticmethod
     def _db_path(path: str):
@@ -15,24 +16,26 @@ class Command(BaseCommand):
         else:
             raise argparse.ArgumentTypeError(f'{path} is not a valid path to an SQLite Database.')
 
-    def add_arguments (self, parser):
-        def add_arguments(self, parser):
-            parser.add_argument('sqlite_file', type=self._db_path)
+    def add_arguments(self, parser):
+        parser.add_argument('sqlite_file', type=self._db_path)
 
     def handle(self, *args, **options):
-        guildID = '480817692350218250'
+        GUILD_ID = '480817692350218250'
         conn = sqlite3.connect((options['sqlite_file']))
         c = conn.cursor()
 
-        self.stdout.write(f'- - Scanning channels')
+        self.stdout.write(f'- Migrating channels...')
         c.execute('SELECT userID, gameName, gameRoom, name')
-        channelList = c.fetchall()
-        self.stdout.write(f'- Found {len(channelList)} channels in the database.')
+        channel_list = c.fetchall()
+        self.stdout.write(f'- - Found {len(channel_list)} channels in the database.')
 
-        for channel in channelList:
+        for channel in channel_list:
             self.stdout.write(f'- - Migrating channel {channel[2]} ({channel[1]})')
             user = User.objects.get_or_create(id=channel[3])[0]
-            Channel.objects.get_or_create(id=channel[2], owner=user, guild_id=guildID, game=channel[1])
+            game = Game.objects.get_or_create(
+                name=channel[1]
+            )[0]
+            Channel.objects.get_or_create(id=channel[2], owner=user, guild_id=GUILD_ID, game=game)
             self.stdout.write(f'- - Migrated channel {channel[2]} successfully.')
-        # Migrate
+
         c.close()
